fba7c544a4ed1c866e1c6d65f0252c9873e0e850
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2021-07-07 13:34:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let counter = document.querySelector("#counter");
+let plusElem = document.querySelector('#plus');
+let minusElem = document.querySelector('#minus');
+let resetCounter = document.querySelector('#reset');
+let increaseBy = document.querySelector('#increaseby');
+let startFrom = document.querySelector('#startfrom');
+incrementer = 1;
+
+plusElem.addEventListener("click", ()=> {
+  counter.innerHTML = +counter.innerHTML+incrementer;
+})
+
+minusElem.addEventListener("click", ()=> {
+  counter.innerHTML = +counter.innerHTML-incrementer;
+})
+
+resetCounter.addEventListener("click", ()=> {
+  let reset = confirm("Are you sure you want to reset the counter?");
+  if(reset == true) {
+    counter.innerHTML = 0;
+  }
+})
+
+increaseBy.addEventListener("click", ()=> {
+  let a = +prompt("Write the base number to increase the counter:", 1);
+  if(isFinite(a)) {
+    incrementer = a;
+  }
+  else {
+    alert("Please write a number");
+  }
+})
+
+startFrom.addEventListener("click", ()=> {
+  let b = +prompt ("Write the number you want to start from:", 0);
+  if(isFinite(b)) {
+    counter.innerHTML = b;
+  }
+  else {
+    alert("Please write a number");
+  }
+})
