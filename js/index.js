3172f8ff96365c7f12d748b8c1291a59946cadc4
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2021-07-11 21:37:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -41,6 +41,10 @@ startFrom.addEventListener("click", ()=> {
     counter.innerHTML = b;
     startFrom.innerHTML = "START FROM: " + b;
   }
+  else if (b===0) {
+    counter.innerHTML = b;
+    startFrom.innerHTML = "START FROM: " + b;
+  }
   else {
     alert("Please write a number");
   }
